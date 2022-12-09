3ffb525fc25caa27c82792c6f70a43d01f597109
Peter Yoachim <yoachim@uw.edu>
2022-12-09 13:21:31 -0800

    testing slurm

---
+++ sdf_slurm/README.md
@@ -0,0 +1,1 @@
+Let's try running a slurm job on sdf
---
+++ sdf_slurm/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+from rubin_sim.scheduler import ModelObservatory
+
+
+if __name__ == '__main__':
+    print('testing from python 1 2 3')
