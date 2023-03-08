78150aa9ffacffc1e1d73cf424f997c4fb4469f5
Peter Yoachim <yoachim@uw.edu>
2023-03-07 21:24:22 -0800

    restore notes

--- restore_time/restore.py
+++ restore_time/restore.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     survey_length = 3653
 
     mo = ModelObservatory(mjd_start=mjd_start)
-    mo, scheduler, observations = sim_runner(mo, scheduler, survey_length=survey_length,
-                                             verbose=True, filename=filename)
+    #mo, scheduler, observations = sim_runner(mo, scheduler, survey_length=survey_length,
+    #                                         verbose=True, filename=filename)
     sched2 = example_scheduler(mjd_start=mjd_start)
     mo = ModelObservatory(mjd_start=mjd_start)
     t1 = time.time()
@@ -27,4 +27,13 @@ if __name__ == '__main__':
     print('Time to restore=%f min' % dt)
 
 
-# 100 days took 2.03 min to restore. So restore a pickle and catch up for a week should be ~10s. If there are daily cehckpoints, ~1.2s
+# 100 days took 2.03 min to restore. So restore a pickle and catch up for a week should be ~10s. If there are daily checkpoints, ~1.2s
+# Time to restore=102.190548 min # this one might have fallen asleep part way through
+
+
+#Flushed 134791 observations from queue for being stale
+#Completed 2087458 observations
+#ran in 200 min = 3.3 hours
+# huh? how did we burn through 10 years in only 3.3 hours? That wild.
+
+# uninteruppted: Time to restore=82.952566 min
