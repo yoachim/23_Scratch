b14819b96f81353538a7b0c3c476234e3a380e40
Peter Yoachim <yoachim@uw.edu>
2023-03-16 16:02:53 -0700

    rough night sim

---
+++ sim_night/sim_night.py
@@ -0,0 +1,43 @@
+import numpy as np
+import copy
+
+
+# How do we go about simulating a night?
+
+# Scenario 1:  Sim night, send the list of observations out, and they all just get executed
+
+# Scenario 2:  Sim night, we get a ways into list, then deviate (clouds roll in, telescope down for an hour, whatever)
+# restore scheduler to point right before divergence. Add in observations that haven't been recorded. 
+# Sim rest of night again.
+
+class NightSimulator(object):
+    """Simulate a night
+
+    Parameters
+    ----------
+    scheduler : rubin_sim.scheduler.Scheduler object
+        A scheduler object. Up to date with all completed observations
+        played into it.
+
+    model_observatory : 
+    """
+    def __init__(self, scheduler, model_observatory):
+        
+        self.scheduler = scheduler
+        self.model_observatory = model_observatory
+
+        self.empty_queue_schedulers = []
+        self.empty_queue_times = []
+
+        scheduler_end, observatory_end, observations_full_night = sim_runner()
+        # Need to generate 
+
+    def update_observatory(self, scheduler_update_object):
+
+        self.model_observatory.update(scheduler_update_object)
+
+    def sim_rest_of_night(self, ):
+        """Simulate the rest of the night"""
+        # Take scheduler with empty queue closest in time to before requested date
+
+
